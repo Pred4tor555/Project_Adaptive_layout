30a48b0987df533d6352527aa07d54e8b1df32d0
Pred4tor555 <artuk200311@mail.ru>
2024-02-22 16:30:25 +0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PhotoArena</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <h1>PhotoArena</h1>
+        <h2>A site where users can upload, view and share photos.</h2>
+        <h3>Kovalenok Artem Alexeyevich, 609-22.</h3>
+    </body>
+</html>
